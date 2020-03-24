e531b3028f3dc0b94f4c434c682e568b94bd3099
Artem Shuba <a.shuba@outlook.com>
2020-03-24 19:02:40 +0100

    Initial Commit

---
+++ StorytelApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StorytelApp
+//
+//  Created by Artem Shuba on 24/03/2020.
+//  Copyright © 2020 Artem Shuba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
